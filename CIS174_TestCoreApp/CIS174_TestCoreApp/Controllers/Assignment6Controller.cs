1b8cc9501df8dacf4946fa225f57158d01384f0b
JohnRoggendorf <jroggendorf@dmacc.edu>
2025-07-01 19:56:36 -0500

    Added new assignment for Razor templates

---
+++ CIS174_TestCoreApp/CIS174_TestCoreApp/Controllers/Assignment6Controller.cs
@@ -0,0 +1,34 @@
+﻿using CIS174_TestCoreApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIS174_TestCoreApp.Controllers
+{
+    public class Assignment6Controller : Controller
+    {
+        [Route("Assignment6/{accessLevel:int}")]
+        public IActionResult Index(int accessLevel)
+        {
+            if (accessLevel < 1 || accessLevel > 10)
+            {
+                return BadRequest("Access level must be between 1 and 10.");
+            }
+
+            // Create sample students
+            var students = new List<Student>
+        {
+            new Student { FirstName = "Aye", LastName = "Person", Grade = "A" },
+            new Student { FirstName = "Psalm", LastName = "Body", Grade = "B+" },
+            new Student { FirstName = "Tess", LastName = "Name", Grade = "A-" },
+            new Student { FirstName = "John", LastName = "Doe", Grade = "B" }
+            };
+
+            var vm = new AssignmentViewModel
+            {
+                Students = students,
+                AccessLevel = accessLevel
+            };
+
+            return View(vm);
+        }
+    }
+}
---
+++ CIS174_TestCoreApp/CIS174_TestCoreApp/Models/AssignmentViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CIS174_TestCoreApp.Models
+{
+    public class AssignmentViewModel
+    {
+        public List<Student> Students { get; set; } = new List<Student>();
+        public int AccessLevel { get; set; }
+    }
+}
