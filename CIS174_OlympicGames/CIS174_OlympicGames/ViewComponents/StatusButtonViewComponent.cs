16f52f4fb4973aad4c9e7331527417c4348c6890
JohnRoggendorf <jroggendorf@dmacc.edu>
2025-07-22 22:29:39 -0500

    Added TagHelper and View Component

---
+++ CIS174_OlympicGames/CIS174_OlympicGames/ViewComponents/StatusButtonViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CIS174_OlympicGames.ViewComponents
+{
+    public class StatusButtonViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string status)
+        {
+            return View("Default", status); //Specify default view name
+        }
+    }
+}
